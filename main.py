3b2ea717d5f7dc24ecbdf72054fcdf2b65b00050
sazurov <sazuroov@yandex.ru>
2023-02-27 14:06:42 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+a = int(input("введите число\t"))
+b = int(input("введите число\t"))
+c = int(input("введите число\t"))
+v = 0
+print("a+b+c=v")
+print(D= b**2+(-4*a*c))
+if D > 0:
+    elife: x1= -b+pow(D)/2*a
+    x2= -b-pow(D)/2*a
+else:
+    if D:=0:
+        elife: x= -b/2*a
+else:
+    if D < 0:
+    elife: print("нет корней")
+print(x)
+print(x1)
+print(x2)
