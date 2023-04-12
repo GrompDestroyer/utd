703841cca0d0e81033440ca03580e582a45dac4f
GrompDestroyer <114862096+GrompDestroyer@users.noreply.github.com>
2023-04-12 15:42:13 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-a = int(input("введите число\t"))
-b = int(input("введите число\t"))
-c = int(input("введите число\t"))
-v = 0
-print("a+b+c=v")
-print(D= b**2+(-4*a*c))
-if D > 0:
-    elife: x1= -b+pow(D)/2*a
-    x2= -b-pow(D)/2*a
+import math
+
+print("Введите коэффициенты для уравнения")
+print("ax^2 + bx + c = 0:")
+a = float(input("a = "))
+b = float(input("b = "))
+c = float(input("c = "))
+
+discr = b ** 2 - 4 * a * c
+print("Дискриминант D = %.2f" % discr)
+
+if discr > 0:
+    x1 = (-b + math.sqrt(discr)) / (2 * a)
+    x2 = (-b - math.sqrt(discr)) / (2 * a)
+    print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
+elif discr == 0:
+    x = -b / (2 * a)
+    print("x = %.2f" % x)
 else:
-    if D:=0:
-        elife: x= -b/2*a
-else:
-    if D < 0:
-    elife: print("нет корней")
-print(x)
-print(x1)
-print(x2)
+    print("Корней нет")
